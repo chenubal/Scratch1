0e89180c6496e6df6462dbcb8214cfaec04e0d9a
jheers <jheers@lavision.de>
2017-06-23 08:09:38 +0200

    Step 2

--- test23/Main.cpp
+++ test23/Main.cpp
@@ -2,25 +2,37 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <vector>
 
 namespace Bank
 {
 	enum class Gender { male, female};
-	static const std::map<Gender, std::string> GenderLabel{ {Gender::male, "mäaennlich"},{ Gender::female, "weiblich"} };
+
+	std::string genderLabel(Gender g)
+	{
+		static const std::map<Gender, std::string> GL{ {Gender::male, "maennlich"},{ Gender::female, "weiblich"} };
+		return GL.count(g)? GL.at(g) :"unknown";
+	}
 
 	struct Kunde
 	{
 		const std::string nachname;
 		const std::string vorname;
 		const Gender geschlecht;
-		Kunde(const std::string& n, const std::string& v, Gender g) : nachname(n) , vorname(v), geschlecht(g) {}
+		float kontostand;
+		Kunde(const std::string& n, const std::string& v, Gender g, float init=0) : nachname(n) , vorname(v), geschlecht(g), kontostand(init) {}
+		void einzahlen(float betrag) { if (betrag > 0) kontostand += betrag; }
+		void auszahlen(float betrag) { if (betrag > 0) kontostand -= betrag; }
+		float Kontostand() const { return kontostand; }
 	};
 
 	std::ostream& operator<<(std::ostream &o, Kunde const& k)
 	{
-		std::cout << "Name: " << k.vorname << " " << k.nachname << "\n" << "Geschlecht: " << GenderLabel.at(k.geschlecht);
+		std::cout << "Name: " << k.vorname << " " << k.nachname << "\n" << "Geschlecht: " << genderLabel(k.geschlecht);
 		return o;
 	}
+
+	using KundenDB = std::vector<Kunde>;
 }
 
 
@@ -28,8 +40,11 @@ int main(int, char**)
 {
 
 	using namespace Bank;
-	Kunde k("Meier","Theo", Gender::male);
-	std::cout << k << "\n";
+	KundenDB kunden = { {"Mueller","Klaus",Gender::male, 45000},
+	                             { "Mueller","Gaby",Gender::female, 4500 },
+								 { "Meier","Tom",Gender::male, 0 } };
+
+	for (auto const& k : kunden) std::cout << k << "\n-------\n";
 	return 0;
 
 }
