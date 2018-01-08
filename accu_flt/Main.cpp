2e3916571796853a6e146f4031d60add7cd57cdd
jheers <jheers@lavision.de>
2018-01-08 13:52:19 +0100

    refactor

--- accu_flt/Main.cpp
+++ accu_flt/Main.cpp
@@ -3,14 +3,6 @@
 #include <numeric>
 #include <functional>
 
-// For printing vectors 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
-{
-	for (auto& el : vec) os << el << ' ';
-	return os;
-}
-
 // Get value type from container type
 template <class C>
 using value_t = typename C::value_type;
@@ -23,7 +15,7 @@ using pred_t = std::function<bool(value_t<C>)>;
 template<class C>
 C filter(C const& cont, pred_t<C> test)
 {
-	auto r = std::accumulate(cont.cbegin(), cont.cend(), C(), [test](C &cr, value_t<C> x)
+	auto r = std::accumulate(cont.cbegin(), cont.cend(), C(), [test](C &cr, value_t<C> x)->C&
 	{
 		if (test(x)) cr.push_back(x);
 		return cr;
@@ -31,11 +23,19 @@ C filter(C const& cont, pred_t<C> test)
 	return r;
 }
 
+// For printing vectors 
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+	for (auto& el : vec) os << el << ' ';
+	return os;
+}
+
 int main()
 {
 	using test_t = int;
 	using testvec_t = std::vector<test_t>;
 
-	testvec_t test{ 1,2,3,4,5,7,8 };
-	std::cout << filter(test, [](test_t x) {return x %2==0;}) << "\n";
+	testvec_t data{ -1, -2,-12, 1,2,3,4,5,7,8, 0 };
+	std::cout << filter(data, [](test_t x) {return x%2==0;}) << "\n";
 }
