c4e5bb971e2f7265a771c665f1be46630688e23c
jheers <jheers@lavision.de>
2018-08-27 16:59:25 +0200

    refactor

--- filter/Main.cpp
+++ filter/Main.cpp
@@ -40,14 +40,14 @@ namespace lv
 	struct container_traits<std::vector<Args...>> : public sequence_container_traits<std::vector<Args...>>
 	{};
 
-	/// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	template<class C>
 	struct associative_container_traits;
 
 	template<template<class, class, class> class C, class T, template<class> class O, class A>
 	struct associative_container_traits<C<T, O<T>, A>>
 	{
-		using value_type = T;
+		using value_type = typename C<T, O<T>, A>::value_type;
 		static void add_element(C<T, O<T>, A>& c, const T& t) { c.insert(t); }
 	};
 
@@ -59,12 +59,19 @@ namespace lv
 	struct container_traits<std::set<Args...>> : public associative_container_traits<std::set<Args...>>
 	{};
 
-	template <class... Args>
-	struct container_traits<std::map<Args...>> 
+	template<template<class, class, class, class> class C, class K, class T, template<class> class O, class A>
+	struct associative_container_traits<C<K, T, O<K>, A>>
 	{
-		using value_type = typename std::map<Args...>::value_type;// std::pair<K, V>;
-		static void add_element(std::map<Args...>& c, const value_type& t) { c.insert(t); }
+		using value_type = typename C<K, T, O<K>, A>::value_type;
+		static void add_element(C<K, T, O<K>, A>& c, const value_type& t) { c.insert(t); }
 	};
+
+	template<class... Args>
+	struct container_traits<std::map<Args...>> : public associative_container_traits<std::map<Args...>>
+	{};
+	template<class... Args>
+	struct container_traits<std::multimap<Args...>> : public associative_container_traits<std::multimap<Args...>>
+	{};
 
 	template<class Container>
 	using FunBVT = std::function<bool(typename container_traits<Container>::value_type)>;
@@ -79,15 +86,15 @@ namespace lv
 	}
 }
 
+using ST = std::set<int>;
+using MT = std::map<int, double>;
 
 int main(int, char**)
 {
-  	using ST = std::set<int>;
 	ST a = { 0, 1,2,3,4,5,6,7,8,9 };
 	for (auto x : lv::filter(a, [](ST::value_type x) {return x % 3 == 0; }))
 		std::cout << x << "\n";
 
-	using MT = std::map<int, double>;
 	MT b = { { 0,10.0 },{ 1,11.0 },{ 2,12.0 },{ 3,13.0 },{ 4,14.0 },{ 5,15.0 },{ 6,16.0 },{ 7,17.0 } };
 	for (auto x : lv::filter(b, [](MT::value_type x)->bool {return x.first % 2 == 0; }))
 		std::cout << x.first << "|" << x.second << "\n";
