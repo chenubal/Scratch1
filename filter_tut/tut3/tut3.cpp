948c831e8e4bb0dfeb2c31542f667fa6bfa9f44f
jheers <jheers@lavision.de>
2018-08-29 15:20:34 +0200

    strings  samples

--- filter_tut/tut3/tut3.cpp
+++ filter_tut/tut3/tut3.cpp
@@ -3,12 +3,14 @@
 #include <list>
 #include <set>
 #include <map>
+#include <string>
 #include <unordered_map>
 
 #include <functional>
 
 namespace jh
 {
+	//--------------------------------------------------------------------------------------------
 	template <class C>
 	struct container_traits
 	{
@@ -16,6 +18,7 @@ namespace jh
 		// void inplace_add(C&,V)
 	};
 
+	//--------------------------------------------------------------------------------------------
 	template<class C>
 	struct sequence_container_traits;
 
@@ -26,7 +29,7 @@ namespace jh
 		static void inplace_add(C<V, A>& c, const V& t) { c.push_back(t); }
 	};
 
-
+	//--------------------------------------------------------------------------------------------
 	template<class C>
 	struct associative_container_traits;
 
@@ -51,21 +54,32 @@ namespace jh
 		static void inplace_add(C<K, V, H<K>, E<K>, A>& c, const value_type& t) { c.insert(t); }
 	};
 
+	//--------------------------------------------------------------------------------------------
+	template<class C>
+	struct string_container_traits;
 
+	template<template<class, class, class> class C, class T, template<class> class K, class A>
+	struct string_container_traits<C<T, K<T>, A>>
+	{
+		using value_type = T;
+		static void inplace_add(C<T, K<T>, A>& c, const T& t) {	c.push_back(t);}
+	};
+
+	//--------------------------------------------------------------------------------------------
 	template<class... Args>
 	struct container_traits<std::deque<Args...>> : public sequence_container_traits<std::deque<Args...>> {};
-
-	template<class... Args>
-	struct container_traits<std::list<Args...>> : public sequence_container_traits<std::list<Args...>> {};
 
 	template<class... Args>
 	struct container_traits<std::vector<Args...>> : public sequence_container_traits<std::vector<Args...>> {};
 
 	template<class... Args>
-	struct container_traits<std::multiset<Args...>> : public associative_container_traits<std::multiset<Args...>> {};
+	struct container_traits<std::list<Args...>> : public sequence_container_traits<std::list<Args...>> {};
 
 	template<class... Args>
 	struct container_traits<std::set<Args...>> : public associative_container_traits<std::set<Args...>>	{};
+
+	template<class... Args>
+	struct container_traits<std::multiset<Args...>> : public associative_container_traits<std::multiset<Args...>> {};
 
 	template<class... Args>
 	struct container_traits<std::map<Args...>> : public associative_container_traits<std::map<Args...>>	{};
@@ -76,12 +90,12 @@ namespace jh
 	template<class... Args>
 	struct container_traits<std::unordered_map<Args...>> : public associative_container_traits<std::unordered_map<Args...>>	{};
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	template<class Container>
-	using FunBVT = std::function<bool(typename container_traits<Container>::value_type)>;
+	template<class... Args>
+	struct container_traits<std::basic_string<Args...>> : public string_container_traits<std::basic_string<Args...>> {};
 
-	template<typename Container>
-	Container filter(Container const& in, FunBVT<Container>&& f)
+	//--------------------------------------------------------------------------------------------
+ 	template<typename Container>
+	Container filter(Container const& in, std::function<bool(typename container_traits<Container>::value_type)>&& f)
 	{
 		Container out;
 		auto add = &container_traits<Container>::inplace_add;
@@ -92,7 +106,14 @@ namespace jh
 	}
 }
 
+//--------------------------------------------------------------------------------------------
+//---------- Tests ---------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+
 #include <iostream>
+
+using namespace std;
+
 
 struct TestStruct
 {
@@ -102,36 +123,41 @@ struct TestStruct
 	operator int() { return int(x); }
 };
 
-std::ostream& operator<<(std::ostream& os, const TestStruct& s)
+ostream& operator<<(ostream& os, const TestStruct& s)
 {
 	os << s.x << '-' << s.y;
 	return os;
 }
 
-using VT = std::vector<TestStruct>;
-using ST = std::set<int>;
-using MT = std::unordered_map<int, double>;
+using VT = vector<TestStruct>;
+using ST = set<int>;
+using MT = unordered_map<int, double>;
 
 int main(int, char**)
 {
+	//--------------------------------------------------------------------------------------------
 	VT v = { 0, 1,2,3,4,5,6,7,8,9 };
-	std::cout << "\nv=";
+	cout << "\nVector = ";
 	auto predicate = [](VT::value_type x) {return int(x) % 2 == 0; };
 	for (auto x : jh::filter(v, predicate))
-		std::cout << x << " ";
+		cout << x << " ";
 
+	//--------------------------------------------------------------------------------------------
 	ST s = { 0, 1,2,3,4,5,6,7,8,9 };
-	std::cout << "\ns=";
+	cout << "\nSet = ";
 	for (auto x : jh::filter(s, [](ST::value_type x) {return int(x) % 3 == 0; }))
-		std::cout << x << " ";
+		cout << x << " ";
 
+	//--------------------------------------------------------------------------------------------
 	MT m = { { 0,11.0 },{ 1,12.0 },{ 2,13.0 },{ 3,14.0 },{ 4,15.0 },{ 5,16.0 },{ 6,17.0 },{ 7,18.0 } };
-	std::cout << "\nm=";
+	cout << "\nMap = ";
 	for (auto x : jh::filter(m, [](MT::value_type x) {return int(x.second) % 2 == 0; }))
-		std::cout << x.first << "|" << x.second << " ";
+		cout << x.first << "|" << x.second << " ";
 
-	std::cout << "\n";
+	//--------------------------------------------------------------------------------------------
+	cout << "\nstring = " << jh::filter(string("dsf_----___das_gjfh---_gdh"), [r=string("_-g")](char c) {return r.find(c)== string::npos; });
+
+	cout << "\n";
 	return 0;
-
 }
 
