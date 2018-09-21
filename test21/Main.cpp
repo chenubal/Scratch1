19f4c6977c543a5ed3328d0667c58c5482641341
jheers <jheers@lavision.de>
2018-09-21 10:57:16 +0200

    Lambda-Iterators

--- test21/Main.cpp
+++ test21/Main.cpp
@@ -1,95 +1,75 @@
 #include <iostream>
 #include <numeric>
 #include <ctime>
+#include <functional>
 
-/// Provides a sequence of natural numbers: n,n+1,n+2,...
+template<class T>
+using eval_fun = std::function<T(unsigned)>;
+
+template<class T>
+struct LambdaIterator
+{
+	LambdaIterator(eval_fun<T> e, unsigned start = 0) : eval(e), counter(start) {}
+	T operator*() const { return eval(counter); }
+	LambdaIterator<T>& operator++() { ++counter; return *this; }
+	LambdaIterator<T> operator++(int) { auto t = counter++; return LambdaIterator<T>(eval, t); }
+	bool operator!=(LambdaIterator<T> const& o) const { return o.counter != counter; }
+	virtual ~LambdaIterator<T>() {}
+protected:
+	unsigned counter;
+	eval_fun<T> eval;
+};
+
+/// /// Provides a sequence of natural numbers: n,n+1,n+2,...
 struct SimpleNumbers
 {
-	using value_type = unsigned;
-	SimpleNumbers(value_type N = 0, value_type start = 0) : N(N), N0(start) {}
-	virtual ~SimpleNumbers() {}
+	SimpleNumbers(unsigned N = 0, unsigned start = 0) : N(N), N0(start) {}
 
-	template<class T = value_type>
-	struct Iterator
-	{
-		explicit Iterator(value_type start = 0) : value(start) {}
-		T operator*() const { return value; }
-		Iterator<T> operator++() { ++value; return *this; }
-		Iterator<T> operator++(int) {auto t = value++; return Iterator<T>(t); }
-		bool operator!=(Iterator<T> const& o) const { return o.value != value; }
-		virtual ~Iterator<T>() {}
-	protected:
-		value_type value;
-	};
-	Iterator<> begin() const { return Iterator<>(N0); }
-	Iterator<> end() const { return Iterator<>(N0 + N); }
-
-protected:
-	const unsigned N;
-	const value_type N0;
+	LambdaIterator<unsigned> begin() const { return LambdaIterator<unsigned>([](unsigned n) { return n; }, N0); }
+	LambdaIterator<unsigned> end() const { return LambdaIterator<unsigned>(nullptr, N0 + N); }
+private:
+	const unsigned N, N0;
 };
 
 /// Provides a sequence of equidistant numbers
 template<class T=int>
-struct Sequence : public SimpleNumbers
+struct Numbers 
 {
-	Sequence(SimpleNumbers::value_type N, T start, T inc = 1) : SimpleNumbers(N), S0(start), DS(inc) { if (N < 2) DS = 0; }
-	Sequence(T start, T end, SimpleNumbers::value_type N) : Sequence(N, start, (end - start) / (N - 1)) { }
-
-	struct Iterator : public SimpleNumbers::Iterator<T>
-	{
-		Iterator(SimpleNumbers::value_type N, T start, T inc) : SimpleNumbers::Iterator<T>(N), s0(start), ds(inc) {}
-		T operator*() const { return  s0 + ds*T(SimpleNumbers::Iterator<T>::value); }
-	private:
-		const T s0, ds;
-	};
-
-	Iterator begin() const { return Iterator(0, S0, DS); }
-	Iterator end()   const { return Iterator(N, 0, 0); }
+	Numbers(unsigned N, T start, T inc = 1) : N(N), S0(start), DS(inc) { if (N < 2) DS = 0; }
+	Numbers(T start, T end, unsigned N) : Numbers(N, start, (end - start) / (N - 1)) { }
+	LambdaIterator<T> begin() const { return LambdaIterator<T>([=](unsigned n)->T { return  S0 + DS*n; }, 0); }
+	LambdaIterator<T> end() const { return LambdaIterator<T>(nullptr, N); }
 private:
 	const T S0;
 	T DS;
+	const unsigned N;
 };
 													    
-
 /// Provides a sequence of random numbers from interval
 template <class T = double>
-struct Rands : public SimpleNumbers
+struct Rands 
 {
-	Rands(SimpleNumbers::value_type N = 1, T scale = T(1)) : SimpleNumbers(N), scale(scale) { std::srand(unsigned(std::time(0))); }
-	struct Iterator : public SimpleNumbers::Iterator<T>
-	{
-		Iterator(SimpleNumbers::value_type N, T scale) : SimpleNumbers::Iterator<T>(N), scale(scale) {}
-		T operator*() const { return T(scale * std::rand() / RAND_MAX); }
-		T scale;
-	};
-
-	Iterator begin() const { return Iterator(0,scale); }
-	Iterator end()   const { return Iterator(N,0); }
+	Rands(unsigned N = 1, T scale = T(1)) : N(N), scale(scale) { std::srand(unsigned(std::time(0))); }
+	LambdaIterator<T> begin() const { return LambdaIterator<T>([=](unsigned )->T {return T(scale * std::rand() / RAND_MAX); }, 0); }
+	LambdaIterator<T> end() const { return LambdaIterator<T>(nullptr, N); }
+private:
 	T scale;
+	const unsigned N;
 };
 
 int main()
 {
 	for (auto x : SimpleNumbers(3)) std::cout << "n=" << x << " ";
 	std::cout << "\n";
-	for (auto x : Sequence<int>(7u, -4, -1)) std::cout << "x=" << x << " ";
+	for (auto x : Numbers<int>(7u, -4, -1)) std::cout << "x=" << x << " ";
 	std::cout << "\n";
-	for (auto x : Sequence<double>(7, -4, 10u)) std::cout << "y=" << x << " ";
+	for (auto x : Numbers<double>(7, -4, 10u)) std::cout << "y=" << x << " ";
 	std::cout << "\n";
-	for (auto x : Rands<int>(10,50)) std::cout << "r=" << x << " ";
+	for (auto x : Rands<float>(10,50)) std::cout << "r=" << x << " ";
 
 	SimpleNumbers h(101);
 	std::cout << "\nSum1=" << std::accumulate(h.begin(), h.end(), 0.0);
-	Sequence<float> g(100u,1);
+	Numbers<float> g(100u,1);
 	std::cout << "\nSum2=" << std::accumulate(g.begin(), g.end(), 0.0);
-
-	SimpleNumbers nats(10,7);
-	auto i = nats.begin();
-	for (auto j : SimpleNumbers(10))
-	{
-		std::cout << "\n" << *(i++);
-
-	}
 
 }
