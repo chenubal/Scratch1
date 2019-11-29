7ad61d4c2d2fb6a83dc3a1a4a0f0615c0c9c25e2
jheers <jheers@lavision.de>
2019-11-29 08:39:15 +0100

    Refactor

--- Visit1/Main.cpp
+++ Visit1/Main.cpp
@@ -2,12 +2,11 @@
 #include <string>
 #include <variant>
 #include <vector>
+#include <unordered_map>
 
 // https://arne-mertz.de/2018/05/overload-build-a-variant-visitor-on-the-fly/
 
 using namespace std;
-
-using ints = vector<int>;
 
 template<typename T>
 ostream& operator<<(ostream& os, const vector<T>& vec)
@@ -16,53 +15,70 @@ ostream& operator<<(ostream& os, const vector<T>& vec)
 	return os;
 }
 
-using attribute = std::variant<int, float, string, ints>;
-using attributes = std::vector<attribute>;
+using ints = vector<int>;
+using attribute_variant = variant<int, double, string, ints>;
+using attribute_t = pair<string, attribute_variant>;
+using attributes_t = unordered_map<string,attribute_variant>;
 
-//struct printer
-//{
-//	void operator()(int i) const { cout << "P-int: " << i << "\n"; }
-//	void operator()(double d) const { cout << "P-double: " << d << "\n"; }
-//	void operator()(string const& s) const { cout << "P-string: " << s << "\n"; }
-//	void operator()(ints const& vi) const { cout << "V-string: " << vi << "\n"; }
-//};
+struct printer
+{
+	printer(ostream& os = std::cout) :os(os) {}
+	void operator()(int i) const { os << "P-int: " << i << "\n"; }
+	void operator()(double d) const { os << "P-double: " << d << "\n"; }
+	void operator()(string const& s) const { os << "P-string: " << s << "\n"; }
+	void operator()(ints const& vi) const { os << "P-ints: " << vi << "\n"; }
+private:
+	ostream& os;
+};
 
-//template<class... Ts>
-//struct overloaded : Ts...
-//{
-//	using Ts::operator()...;
-//};
-//template<class... Ts>
-//overloaded(Ts...)->overloaded<Ts...>;
+void print(attribute_variant const& a, ostream& os = std::cout)
+{
+	static printer p(os);
+	visit(p, a);
+}
+
+template<class... Ts>
+struct overloaded : Ts...
+{
+	using Ts::operator()...;
+};
+template<class... Ts>
+overloaded(Ts...)->overloaded<Ts...>;
 
 
 int main() 
 {
 
-	attributes vec = { 10, 1.5f, "hello", ints{7,8,9} };
-	if (auto pval = get_if<int>(&vec[1]))
-		cout << "variant ist ein <int>: " << *pval << '\n';
+	attributes_t attributes = { {"one",10}, {"two",1.5}, {"three","hello"}, {"four",ints{7,8,9}} };
+
+	cout << "***** simple access ************************************************\n";
+	if (auto pa = get_if<int>(&attributes["one"]))
+		cout << "variant<int>: value=" << *pa << '\n';
 	else
-		cout << "variant ist kein <int>" << '\n';
+		cout << "no variant<int>" << '\n';
 
-	cout << "*****************************************************\n";
-	for (auto const& v : vec)
-		visit([](auto&& x) {cout << x << "\n"; }, v);
+	cout << "***** with auto lambda ************************************************\n";
+	auto lprint = [](attribute_t const& attr)
+	{
+		visit([name = attr.first](auto&& value) {cout << name << "=" << value << "\n"; }, attr.second);
+	};
+	for (auto const& attr : attributes)
+		lprint(attr);
 
-	//cout << "*****************************************************\n";
-	//for (auto&& v : vec)
-	//	visit(printer(), v);
+	cout << "***** with printer class ************************************************\n";
+	for (auto const& attr : attributes)
+		print(attr.second);
 
-	//cout << "*****************************************************\n";
-	//for (auto& v : vec)
-	//{
-	//	visit(overloaded
-	//		{
-	//			[](auto    arg) { cout << "auto:   " << arg << "\n"; },
-	//			[](double  arg) { cout << "double: " << arg << "\n"; },
-	//			[](string& arg) { cout << "string: " << arg << "\n"; },
-	//			[](ints& arg) { cout << "ints: " << arg << "\n"; },
-	//		}
-	//	, v);
-	//}
+	cout << "***** with lambda overload ************************************************\n";
+	for (auto const& attr : attributes)
+	{
+		visit(overloaded
+			{
+				[](auto   x) { cout << "auto:   " << x << "\n"; },
+				[](double x) { cout << "double: " << x << "\n"; },
+				[](string x) { cout << "string: " << x << "\n"; },
+				[](ints const&  x) { cout << "ints:   " << x << "\n"; },
+			}
+		, attr.second);
+	}
 }
