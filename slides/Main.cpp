8b5e814c2add9e110083d2983e0507f58494f1b1
jheers <jheers@lavision.de>
2021-03-04 15:51:17 +0100

    fix for gcc

--- slides/Main.cpp
+++ slides/Main.cpp
@@ -4,53 +4,58 @@
 #include <utility>
 #include <algorithm>
 
-template<class C, class I>
-auto iterator_n(C &c, I n) { auto i = c.begin(); std::advance(i, n); return i; }
+template<class Iterable, class I>
+auto iterator_n(Iterable &c, I n) { auto iter = c.begin(); std::advance(iter, n); return iter; }
 
-template<class C>
+using index_t = unsigned long;
+
+template<class Iterable>
 struct crop
 {
-	using iterator = typename C::iterator;
+	using iterator = typename Iterable::iterator;
 	using const_iterator = const iterator;
-	using value_type = typename C::value_type;
-	using index_t = unsigned;
+	using value_type = typename Iterable::value_type;
 
-	crop(C & c, index_t i0, index_t iN) 
-	{ 
+	crop(Iterable & c, index_t i0, index_t iN)
+	{
 		s = iterator_n(c, std::min<index_t>(i0, index_t(c.size())));
-		e = iterator_n(c, std::min<index_t>(iN+1, index_t(c.size())));
+		e = iterator_n(c, std::min<index_t>(iN + 1, index_t(c.size())));
 	}
-	crop(C & c, index_t i0 = 0) : crop(c, i0, index_t(c.size()) - 1) {}
+	crop(Iterable const& c, index_t i0, index_t iN)
+	{
+		s = iterator_n(c, std::min<index_t>(i0, index_t(c.size())));
+		e = iterator_n(c, std::min<index_t>(iN + 1, index_t(c.size())));
+	}
+	crop(Iterable & c, index_t i0 = 0) : crop(c, i0, index_t(c.size()) - 1) {}
+	crop(Iterable const& c, index_t i0 = 0) : crop(c, i0, index_t(c.size()) - 1) {}
+	crop() = default;
 
 	iterator begin() { return (s); }
 	iterator end() { return (e); }
 	const_iterator begin() const { return (s); }
 	const_iterator end()   const { return (e); }
-	auto size() const {return std::distance(begin(), end());}
+	auto size() const { return std::distance(begin(), end()); }
 private:
 	iterator s, e;
 };
 
-template<class C>
+template<class Iterable>
 struct slice
 {
-	using index_t = unsigned;
-	using value_type = typename C::value_type;
-	using container_iterator = std::conditional_t<std::is_const_v<C>, typename C::const_iterator, typename C::iterator>;
-	struct iterator 
+	using value_type = typename Iterable::value_type;
+	struct iterator
 	{
 		using iterator_category = std::forward_iterator_tag;
 		using difference_type = std::ptrdiff_t;
 		using value_type = slice::value_type;
-		using pointer = slice::container_iterator;
-		using reference = value_type&;
+		using pointer = std::conditional_t<std::is_const<Iterable>::value, typename Iterable::const_iterator, typename Iterable::iterator>;
+		using reference = value_type & ;
 
 		iterator() = default;
-		iterator(pointer p, index_t d=1) : m_iter(p), dist(d) {}
+		iterator(pointer p, index_t d = 1) : m_iter(p), dist(d) {}
 		reference operator*() const { return *m_iter; }
-		pointer operator->() { return m_iter; }
-		iterator& operator++() { std::advance(m_iter,dist); return *this; }
-		iterator operator++(int) { iterator tmp = *this; ++(*this); return tmp; }
+		iterator& operator++() { std::advance(m_iter, dist); return *this; }
+		iterator operator++(int) { return std::exchange(*this, ++(*this)); }
 		friend bool operator== (const iterator& a, const iterator& b) { return a.m_iter == b.m_iter; };
 		friend bool operator!= (const iterator& a, const iterator& b) { return !(a == b); };
 
@@ -60,11 +65,18 @@ struct slice
 	};
 	using const_iterator = const iterator;
 
-	slice(C & c, index_t d = 1): dist(d)
+	slice(Iterable& c, index_t d = 1) : dist(d)
 	{
 		s = { iterator_n(c, 0), dist };
 		e = { iterator_n(c, (index_t(c.size()) / dist)*dist),dist };
 	}
+	slice(Iterable const& c, index_t d = 1) : dist(d)
+	{
+		s = { iterator_n(c, 0), dist };
+		e = { iterator_n(c, (index_t(c.size()) / dist)*dist),dist };
+	}
+	slice() = default;
+
 	iterator begin() { return s; }
 	iterator end() { return e; }
 	const_iterator begin() const { return s; }
@@ -77,25 +89,32 @@ private:
 
 
 template<class T>
-auto makeNumberGen(T start = T(0), T inc = T(1))
+auto makeNumberGenerator(T start = T(0), T inc = T(1))
 {
 	return [v = start, i = inc]()mutable->T {return std::exchange(v, v + i); };
 }
 
-
 int main()
 {
-	std::vector<double> vd(20);
-	std::generate(vd.begin(), vd.end(), makeNumberGen<double>());
+	using V = std::vector<double>;
+	V vd(20);
+	std::generate(vd.begin(), vd.end(), makeNumberGenerator<double>());
 
-	for (auto&& x : crop(vd, 3, 7))  std::cout << x << "\n";
-	std::cout << "---------\n";
-	for (auto&& x : slice(vd, 2))  std::cout << x << "\n";
-	std::cout << "---------\n";
-	for (auto&& x : crop(slice(vd, 2), 2))  std::cout << x << "\n";
-	std::cout << "---------\n";
-	for (auto&& x : slice(crop(vd, 2), 3))  std::cout << x << "\n";
-	for (auto&& x : slice(crop(vd, 2), 3)) x=99;
-	std::cout << "---------\n";
-	for (auto&& x : vd)  std::cout << x << "\n";
+	for (auto&& x : vd)  std::cout << x << " ";
+	std::cout << "\n---------\n";
+
+	for (auto&& x : crop(vd, 3, 7)) std::cout << x << " ";
+	std::cout << "\n---------\n";
+
+	for (auto&& x : slice(vd, 2)) std::cout << x << " ";
+	std::cout << "\n---------\n";
+
+	for (auto&& x : crop(slice(vd, 2), 2)) std::cout << x << " ";
+	std::cout << "\n---------\n";
+
+	for (auto&& x : slice(crop(vd, 2), 3)) std::cout << x << " ";
+	for (auto&& x : slice(crop(vd, 2), 3)) x = 99;
+	std::cout << "\n---------\n";
+
+	for (auto&& x : vd)  std::cout << x << " ";
 }
