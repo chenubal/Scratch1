eae8631d920eeda98ffd85b6487a418e3f3e784b
jheers <jheers@lavision.de>
2021-03-10 14:37:47 +0100

    Refactor

--- slides/Main.cpp
+++ slides/Main.cpp
@@ -4,42 +4,43 @@
 #include <utility>
 #include <algorithm>
 
-template<class C, class I>
-auto iterator_n(C &c, I n) { auto iter = c.begin(); std::advance(iter, n); return iter; }
-
 using index_t = unsigned long;
 
-template<class C, bool is_const>
+template<class Iterable>
+auto iterator_n(Iterable &c, index_t n) { auto iter = c.begin(); std::advance(iter, n); return iter; }
+
+template<class Iterable, bool is_const>
 struct crop
 {
-	using iterator = std::conditional_t<is_const, typename C::const_iterator, typename C::iterator>;
+	using iterator = std::conditional_t<is_const, typename Iterable::const_iterator, typename Iterable::iterator>;
 	using const_iterator = const iterator;
-	using value_type = typename C::value_type;
-	iterator s, e;
+	using value_type = typename Iterable::value_type;
 
 	template<class S>
-	crop(S & c, index_t i0, index_t iN)
+	crop(S & c, index_t first, index_t last)
 	{
-		s = iterator_n(c, std::min<index_t>(i0, index_t(c.size())));
-		e = iterator_n(c, std::min<index_t>(iN + 1, index_t(c.size())));
+		begin_ = iterator_n(c, std::min<index_t>(first, index_t(c.size())));
+		end_ = iterator_n(c, std::min<index_t>(last + 1, index_t(c.size())));
 	}
 	template<class S>
-	crop(S&& c, index_t i0, index_t iN)
+	crop(S&& c, index_t first, index_t last)
 	{
-		s = iterator_n(c, std::min<index_t>(i0, index_t(c.size())));
-		e = iterator_n(c, std::min<index_t>(iN + 1, index_t(c.size())));
+		begin_ = iterator_n(c, std::min<index_t>(first, index_t(c.size())));
+		end_ = iterator_n(c, std::min<index_t>(last + 1, index_t(c.size())));
 	}
 	template<class S>
-	crop(S & c, index_t i0 = 0) : crop(c, i0, index_t(c.size()) - 1) {}
+	crop(S & c, index_t first = 0) : crop(c, first, index_t(c.size()) - 1) {}
 	template<class S>
-	crop(S&& c, index_t i0 = 0) : crop(c, i0, index_t(c.size()) - 1) {}
+	crop(S&& c, index_t first = 0) : crop(c, first, index_t(c.size()) - 1) {}
 	crop() = default;
 
-	auto begin() { return s; }
-	auto end() { return e; }
-	auto begin() const { return s; }
-	auto end()   const { return e; }
-	auto size() const { return std::distance(s, e); }
+	auto begin() { return begin_; }
+	auto end()   { return end_; }
+	auto begin() const { return begin_; }
+	auto end()   const { return end_; }
+	auto size() const { return std::distance(begin_, end_); }
+private:
+	iterator begin_, end_;
 };
 
 // User CTAD
@@ -56,16 +57,16 @@ crop(S&&, index_t)->crop<S, std::is_const_v<std::remove_reference_t<S>>>;
 template <typename S>
 crop(S&&)->crop<S, std::is_const_v<std::remove_reference_t<S>>>;
 
-template<class C, bool is_const>
+template<class Iterable, bool is_const>
 struct slice
 {
-	using value_type = typename C::value_type;
+	using value_type = typename Iterable::value_type;
 	struct iterator
 	{
 		using iterator_category = std::forward_iterator_tag;
 		using difference_type = std::ptrdiff_t;
 		using value_type = slice::value_type;
-		using pointer = std::conditional_t<is_const, typename C::const_iterator, typename C::iterator>;
+		using pointer = std::conditional_t<is_const, typename Iterable::const_iterator, typename Iterable::iterator>;
 		using reference = value_type & ;
 
 		iterator() = default;
@@ -83,29 +84,30 @@ struct slice
 	using const_iterator = const iterator;
 
 	template<class S>
-	slice(S& c, index_t d = 1) : dist(d)
+	slice(S& c, index_t step = 1) : step_(step)
 	{
-		s = { iterator_n(c, 0), dist };
-		e = { iterator_n(c, (index_t(c.size()) / dist)*dist),dist };
+		begin_ = { iterator_n(c, 0), step_ };
+		end_ = { iterator_n(c, (index_t(c.size()) / step_)*step_),step_ };
 	}
 	template<class S>
-	slice(S&& c, index_t d = 1) : dist(d)
+	slice(S&& c, index_t step = 1) : step_(step)
 	{
-		s = { iterator_n(c, 0), dist };
-		e = { iterator_n(c, (index_t(c.size()) / dist)*dist),dist };
+		begin_ = { iterator_n(c, 0), step_ };
+		end_ = { iterator_n(c, (index_t(c.size()) / step_)*step_),step_ };
 	}
 	slice() = default;
 
-	auto begin() { return s; }
-	auto end() { return e; }
-	auto begin() const { return s; }
-	auto end()   const { return e; }
-	auto size() const { return std::distance(s, e); }
+	auto begin() { return begin_; }
+	auto end()   { return end_; }
+	auto begin() const { return begin_; }
+	auto end()   const { return end_; }
+	auto size()  const { return std::distance(begin_, end_); }
 private:
-	index_t dist = 1;
-	iterator s, e;
+	index_t step_ = 1;
+	iterator begin_, end_;
 };
 
+// User CTAD
 template <typename S>
 slice(S&, index_t)->slice<S, std::is_const_v<std::remove_reference_t<S>>>;
 template <typename S>
@@ -128,9 +130,13 @@ int main()
 	std::cout << "\n---------\n";
 	for (auto && x : crop(slice(v, 2), 2)) std::cout << x << " ";
 	std::cout << "\n---------\n";
-	// Test: modifying vector: nd combined ops
+	// Test: modifying vector with combined ops
 	V  w{ 1,2,3,4,5,6,7,8,9,0 };
 	for (auto && x : slice(crop(w, 2), 2)) x = 99;
+	for (auto const& x : w) std::cout << x << " ";
+	std::cout << "\n---------\n";
+	auto sc= slice(crop(w, 3), 2);
+	std::for_each(sc.begin(), sc.end(), [](auto &x) {x = 77; });
 	for (auto const& x : w) std::cout << x << " ";
 	std::cout << "\n---------\n";
 }
