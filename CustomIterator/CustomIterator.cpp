e5aa6812836993405b3600215bad4a06a2249f91
jheers <jheers@lavision.de>
2021-08-31 08:49:30 +0200

    Refactor

--- CustomIterator/CustomIterator.cpp
+++ CustomIterator/CustomIterator.cpp
@@ -1,80 +1,103 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 
-template<class T>
-struct MyContainer
+namespace JH
 {
-	MyContainer(size_t n = 0) : d(n) {}
-	MyContainer(MyContainer const&) = default;
-
-	template<bool is_const = true>
-	struct iterator 
+	template<class S, bool is_const = true>
+	struct Iterator
 	{
 		using iterator_category = std::forward_iterator_tag;
 		using difference_type = std::ptrdiff_t;
-		using value_type = std::conditional_t<is_const, T const, T>;
+		using value_type = std::conditional_t<is_const, S const, S>;
 		using pointer = value_type*;
 		using reference = value_type&;
 
-		iterator(T* d, size_t n) : d(d), n(n) {}
-		iterator(T const* d, size_t n) : d(d), n(n) {}
+		Iterator(S* d, size_t n) : d(d), n(n) {}
+		Iterator(S const* d, size_t n) : d(d), n(n) {}
 
 		reference operator*() { return d[n]; }
-		pointer operator->() { return d+n; }
+		pointer operator->() { return d + n; }
 		auto& operator++() { n++; return *this; }
 		auto operator++(int) { auto t = *this; ++(*this); return t; }
-		friend bool operator== (const iterator& a, const iterator& b) { return a.n == b.n; }
-		friend bool operator!= (const iterator& a, const iterator& b) { return !(a == b); }
+		friend bool operator== (const Iterator& a, const Iterator& b) { return a.n == b.n; }
+		friend bool operator!= (const Iterator& a, const Iterator& b) { return !(a == b); }
 		size_t n;
 		pointer d;
 	};
 
-	auto begin() { return iterator<false>(d.data(), 0); }
-	auto end() { return iterator<false>(d.data(), d.size()); }
-	auto begin() const { return iterator<true>(d.data(), 0); }
-	auto end() const { return iterator<true>(d.data(), d.size()); }
 
-protected:
-	std::vector<T> d;
-};
-
-
-int main()
-{
-	auto printJH = [](const char* s, auto const& C) {std::cout << s << " = "; for (auto const& x : C) { std::cout << x.a << " "; } std::cout << "\n"; };
-	int i = 10;
-	struct base_type
+	template<class T>
+	struct Host
 	{
-		bool operator==(base_type const& bt) { return a == bt.a; }
+		using value_type = T;
+
+		explicit Host(size_t n = 0) : d(n) {}
+		Host(Host const&) = default;
+
+		auto begin() { return Iterator<value_type,false>(d.data(), 0); }
+		auto end() { return Iterator<value_type,false>(d.data(), d.size()); }
+		auto begin() const { return Iterator<value_type,true>(d.data(), 0); }
+		auto end() const { return Iterator<value_type,true>(d.data(), d.size()); }
+
+		auto size() const { return d.size(); }
+		auto& at(size_t i) { return d.at(i); }
+		auto& operator[](size_t i) { return d[i]; }
+
+	protected:
+		std::vector<value_type> d;
+	};
+
+	struct TestClient
+	{
+		bool operator==(TestClient const& bt) { return a == bt.a; }
 		bool operator==(int x) { return a == x; }
 		int a = 0; double b = 0;
 	};
 
-	MyContainer<base_type> C(5);
+	using TestClass = Host<TestClient>;
+}
 
-	for (auto& x : C) { x.a = i++; }
-	printJH("C", C);
+using namespace JH;
 
-	// CC(C)
-	const MyContainer< base_type> CC(C);
-	printJH("CC", CC);
+int main()
+{
+	auto print = [](std::string const& prefix, auto const& container, std::ostream& os=std::cout) 
+	{
+		os << prefix << " = "; 
+		for (auto const& e : container) os << e.a << " ";  
+		os << "\n"; 
+	};
 
-	// C += 20
-	std::for_each(C.begin(), C.end(), [](auto& x) { x.a += 20; });
-	printJH("C+=20", C);
+	TestClass tc(5);
 
-	// C = CC
-	std::transform(CC.begin(), CC.end(), C.begin(), [](auto& x) { return x; });
-	printJH("C=CC", C);
+	using V = std::decay_t<decltype(tc.at(0))>; // :-)
 
-	if (auto p = std::find(C.begin(), C.end(), 12); p != C.end())
+	// tc = 10,11,....
+	std::generate(tc.begin(), tc.end(), [k = 10]() mutable {return  V{ k++, 0.0 };
+});
+	print("tc", tc);
+
+	// tcc(tc)
+	const JH::TestClass tcc(tc);
+	print("tcc", tcc);
+
+	// tc += 20
+	std::for_each(tc.begin(), tc.end(), [](auto& e) { e.a += 20; });
+	print("tc+=20", tc);
+
+	// tc = CC
+	std::transform(tcc.begin(), tcc.end(), tc.begin(), [](auto& e) { return e; });
+	print("tc=tcc", tc);
+
+	// Use it->
+	if (auto p = std::find(tc.begin(), tc.end(), 12); p != tc.end())
 		p->a = 77;
-	printJH("C12-77", C);
+	print("tc(12)=77", tc);
 
-	if (auto p = std::find_if(C.begin(), C.end(), [](auto x) { return x.a==13;}); p != C.end())
+	// Use *it
+	if (auto p = std::find_if(tc.begin(), tc.end(), [](auto const& e) { return e.a==13;}); p != tc.end())
 		(*p).a = 66;
-	printJH("C13=66", C);
+	print("tc(13)=66", tc);
 
 }
