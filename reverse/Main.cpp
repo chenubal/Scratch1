9ce22b8df1e3cec1e8d3612e3d0fd57b5aa02de8
jheers <jheers@lavision.de>
2022-05-24 13:02:17 +0200

    variant storage

--- reverse/Main.cpp
+++ reverse/Main.cpp
@@ -2,46 +2,111 @@
 #include <string>
 #include <vector>
 #include <type_traits>
+#include <variant>
 
-using Ints = std::vector<int>;
-
-template<class ReverseIterable>
-struct reverseAdapter
+namespace jh
 {
-	reverseAdapter(ReverseIterable& r) : ref(r) {}
+	template<typename T>
+	using RW = std::reference_wrapper<T>;
+	template<typename T>
+	using RWC = std::reference_wrapper<T const>;
 
-	auto begin() { return std::rbegin(ref); }
-	auto end() { return std::rend(ref); }
-	auto begin() const { return std::rbegin(ref); }
-	auto end() const { return std::rend(ref); }
-	ReverseIterable& ref;
-};
+	template<typename T>
+	struct Value
+	{
+		T data;
+		explicit Value(T&& v) : data(std::move(v)) {}
+	};
 
-template<class C>
-void print(C const& container)
-{
-	for (auto const& v : container)
-		std::cout << v << ", ";
-	std::cout << std::endl;
+	template<typename T>
+	using Storage = std::variant<Value<T>, RWC<T>, RW<T>>;
+
+	template<typename... Functions>
+	struct overload : Functions...
+	{
+		using Functions::operator()...;
+		overload(Functions... functions) : Functions(functions)... {}
+	};
+
+	template<typename T>
+	T const& getConstReference(Storage<T> const& storage)
+	{
+		return std::visit(
+			overload(
+				[](Value<T> const& x) -> T const& { return x.data; },
+				[](RW<T> const& x) -> T const& { return x.get(); },
+				[](RWC<T> const& x) -> T const& { return x.get(); }
+		), storage);
+	}
+
+	template<typename T>
+	T& getReference(Storage<T>& storage)
+	{
+		return std::visit(
+			overload(
+				[](Value<T>& x) -> T& { return x.data; },
+				[](RW<T>& x) -> T& { return x.get(); },
+				[](RWC<T>&)->T& { throw std::runtime_error{ "invalid conversion" }; }
+		), storage);
+	}
+
+	template <typename T>
+	struct reverse
+	{
+		explicit reverse(T& value) : storage(std::ref(value)) {}
+		explicit reverse(T const& value) : storage(std::cref(value)) {}
+		explicit reverse(T&& value) : storage(Value(std::move(value))) {}
+
+		T& data() { return getReference(storage); }
+		T const& data() const { return getConstReference(storage); }
+
+		auto begin() { return std::rbegin(data()); }
+		auto end() { return std::rend(data()); }
+		auto begin() const {	return std::rbegin(data());}
+		auto end() const { return std::rend(data()); }
+	private:
+		Storage<T> storage;
+	};
 }
 
-template<class C>
-void print_r(C const& container)
-{
-	print(reverseAdapter(container));
-}
 
 int main()
 {
-	Ints values = {1,2,3,4,5 };
+	using Ints = std::vector<int>;
 
-	for (auto && v : values) v += 10;
-	print(values);
+	auto print = [](auto const& container) {for (auto const& v : container)	std::cout << v << ", ";	std::cout << std::endl; };
+	auto print_r = [&](auto const& container) { std::cout << "R: "; print(jh::reverse(container));};
 
-	for (auto && v : reverseAdapter(values)) v += 10;
-	print_r(values);
+	{
+		std::cout << "lvalue\n";
+		Ints values = { 1,2,3,4,5 };
 
-	double a[] = { -5.1, 10.2, 15.3 };
-	print_r(a);
+		for (auto && v : values) v += 10;
+		print(values);
+
+		for (auto && v : jh::reverse(values)) v += 10;
+		print_r(values);
+	}
+
+	{
+		std::cout << "lvalue const\n";
+		const Ints values = { 1,2,3,4,5 };
+		print(values);
+		print_r(values);
+	}
+
+	{
+		std::cout << "rvalue\n";
+		print_r(Ints{ 270, 280, 290 });
+		print_r(std::initializer_list<int>{ 770, 880, 990 });
+
+		for (auto const& v : jh::reverse(std::initializer_list<int>{ 70, 80, 90 }))
+			std::cout << v << ", ";
+		std::cout << std::endl;
+
+		for (auto const& v : jh::reverse(Ints{ 370, 380, 390 }))
+			std::cout << v << ", ";
+		std::cout << std::endl;
+	}
 
 }
