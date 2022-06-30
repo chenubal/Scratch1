064af939be640352afa4cd2dee7b50df4afde761
Josef Heers <jheers@lavision.de>
2022-06-30 16:38:13 +0200

    update

--- NamedTypes/Main.cpp
+++ NamedTypes/Main.cpp
@@ -33,27 +33,25 @@ namespace jh
 
 	std::string unit(AngleType a)
 	{
-		return std::visit(
-			overload(
-				[](Angle) { return std::string("\370"); },
-				[](Radiant) { return std::string("rad"); }
-		), a);
+		auto v = overload([](Angle) { return std::string("\370"); },[](Radiant) { return std::string("rad"); });
+		return std::visit( v, a);
 	}
 
-	double factor(AngleType l)
+	double factor(AngleType a)
 	{
-		return std::visit(
-			overload(
-				[](Angle ) { return 1.0; },
-				[](Radiant ) { return 180.0/PI; }
-		), l);
+		auto v = overload( [](Angle) { return 1.0; }, [](Radiant) { return 180.0 / PI; });
+		return std::visit(v, a);
 	}
 
 	template<class T = Angle>
-	AngleType convert(AngleType l)
+	AngleType convert(AngleType a)
 	{
-		auto q = factor(l) / factor(AngleType(T(0)));
-		return T(std::visit([&](auto l) { return  *l * q; }, l));
+		if (!std::holds_alternative<T>(a))
+		{
+			auto q = factor(a) / factor(AngleType(T(0)));
+			a = T(std::visit([&](auto l) { return  *l * q; }, a));
+		}
+		return a;
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
@@ -65,31 +63,33 @@ namespace jh
 
 	std::string unit(LengthType l)
 	{
-		return std::visit(
-			overload(
-				[](LengthMM) { return std::string("mm"); },
-				[](LengthM) { return std::string("m"); },
-				[](LengthKM) { return std::string("km"); },
-				[](LengthMiles) { return std::string("mil"); }
-		), l);
+		auto v = overload(
+			[](LengthMM) { return std::string("mm"); },
+			[](LengthM) { return std::string("m"); },
+			[](LengthKM) { return std::string("km"); },
+			[](LengthMiles) { return std::string("mil"); });
+		return std::visit(v, l);
 	}
 
 	double factor(LengthType l)
 	{
-		return std::visit(
-			overload(
-				[](LengthMM l) { return 0.001; },
-				[](LengthM l) { return 1.0; },
-				[](LengthKM l) { return 1000.0; },
-				[](LengthMiles l) { return 1609.34; }
-		), l);
+		auto v = overload(
+			[](LengthMM l) { return 0.001; },
+			[](LengthM l) { return 1.0; },
+			[](LengthKM l) { return 1000.0; },
+			[](LengthMiles l) { return 1609.34; });
+		return std::visit(v, l);
 	}
 
 	template<class T = LengthM>
 	LengthType convert(LengthType l)
 	{
-		auto q = factor(l) / factor(LengthType(T(0)));
-		return T(std::visit([&](auto l) { return  *l * q; }, l));
+		if (!std::holds_alternative<T>(l))
+		{
+			auto q = factor(l) / factor(LengthType(T(0)));
+			l = T(std::visit([&](auto l) { return  *l * q; }, l));
+		}
+		return l;
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
@@ -101,12 +101,11 @@ namespace jh
 
 	std::string unit(Temperature t)
 	{
-		return std::visit(
-			overload(
-				[](Kelvin) { return std::string("\370K"); },
+		auto v =	overload(
+				[](Kelvin) { return std::string("K"); },
 				[](Celsius) { return std::string("\370C"); },
-				[](Fahrenheit) { return std::string("\370F"); }
-		), t);
+				[](Fahrenheit) { return std::string("\370F"); }	);
+		return std::visit(v, t);
 	}
 
 	///Temperature conversion K = C + 270; F = C*1.8 + 32
@@ -114,39 +113,44 @@ namespace jh
 	/// Scaling factor w.r.t Celsius
 	double factor(Temperature t)
 	{
-		return std::visit(
-			overload(
+		auto v =	overload(
 				[](Kelvin) { return 1.0; },
 				[](Celsius) { return 1.0; },
-				[](Fahrenheit) { return 1.8; }
-		), t);
+				[](Fahrenheit) { return 1.8; });
+		return std::visit(v, t);
 	}
 
 	/// Scaling offset w.r.t Celsius
 	double offset(Temperature t)
 	{
-		return std::visit(
-			overload(
+		auto v =	overload(
 				[](Kelvin) { return 271.0; },
 				[](Celsius) { return 0.0; },
-				[](Fahrenheit) { return 32.0; }
-		), t);
+				[](Fahrenheit) { return 32.0; });
+		return std::visit(v, t);
 	}
 
-	Temperature convertC(Temperature t)
+	Temperature toCelsius(Temperature t)
 	{
-		auto q = factor(t);
-		auto o = offset(t);
-		return Celsius(std::visit([&](auto l) { return  ((*l) - o) / q; }, t));
+		if (!std::holds_alternative<Celsius>(t))
+		{
+			auto q = factor(t);
+			auto o = offset(t);
+			t = Celsius(std::visit([&](auto l) { return  ((*l) - o) / q; }, t));
+		}
+		return t;
 	}
 
 	template<class T = Celsius>
 	Temperature convert(Temperature t)
 	{
-		auto tc = convertC(t);
-		auto q = factor(T(0));
-		auto o = offset(T(0));
-		return T(std::visit([&](auto l) { return  (*l) * q  + o ; }, tc));
+		if (!std::holds_alternative<T>(t))
+		{
+			auto q = factor(T(0));
+			auto o = offset(T(0));
+			t = T(std::visit([&](auto l) { return  (*l) * q + o; }, toCelsius(t)));
+		}
+		return t;
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
@@ -158,7 +162,7 @@ namespace jh
 	}
 
 	template<class T>
-	std::string toStr(T l)
+	std::string asLabel(T l)
 	{
 		return std::to_string(value(l)) + unit(l);
 	}
@@ -169,22 +173,23 @@ using namespace jh;
 template<class T = LengthM>
 void print(LengthType l)
 {
-	std::cout << "Length: " << toStr(convert<T>(l)) << "\n";
+	std::cout << "Length: " << asLabel(convert<T>(l)) << "\n";
 }
 
 #define NL std::cout <<"\n";
 
 int main(int, char**)
 {
+	auto test = [](std::string const l, auto v) {	std::cout << l << " " << v << "\n";	};
 	jh::AngleType x = Angle(45);
-	std::cout << "angle: " << toStr(x) << "\n";
-	std::cout << "angle: " << value(x) << "\n";
+	test("angle:", asLabel(x));
+	test("angle:", value(x));
 	NL
-	std::cout << "angle: " << toStr(convert<Angle>(x)) << "\n";
-	std::cout << "angle: " << toStr(convert<Radiant>(x)) << "\n";
+	test("angle:", asLabel(convert<Angle>(x)));
+	test("angle:", asLabel(convert<Radiant>(x)));
 	x = Radiant(jh::PI/4.0);
-	std::cout << "angle: " << toStr(convert<Angle>(x)) << "\n";
-	std::cout << "angle: " << toStr(convert<Radiant>(x)) << "\n";
+	test("angle:", asLabel(convert<Angle>(x)));
+	test("angle:", asLabel(convert<Radiant>(x)));
 	NL
 	print(LengthM(11));
 	print(LengthMiles(2));
@@ -192,19 +197,19 @@ int main(int, char**)
 	print<LengthKM>(LengthMiles(2));
 	NL
 	Temperature t = Celsius(30.0);
-	std::cout << "temperature: " << toStr(convert<Celsius>(t)) << "\n";
-	std::cout << "temperature: " << toStr(convert<Kelvin>(t)) << "\n";
-	std::cout << "temperature: " << toStr(convert<Fahrenheit>(t)) << "\n";
+	test("temperature:", asLabel(convert<Celsius>(t)));
+	test("temperature:", asLabel(convert<Kelvin>(t)));
+	test("temperature:", asLabel(convert<Fahrenheit>(t)));
 	NL
 	t = Fahrenheit(86);
-	std::cout << "temperature: " << toStr(convert<Celsius>(t)) << "\n";
-	std::cout << "temperature: " << toStr(convert<Kelvin>(t)) << "\n";
-	std::cout << "temperature: " << toStr(convert<Fahrenheit>(t)) << "\n";
+	test("temperature:", asLabel(convert<Celsius>(t)) );
+	test("temperature:", asLabel(convert<Kelvin>(t)) );
+	test("temperature:", asLabel(convert<Fahrenheit>(t)));
 	NL
 	t = Kelvin(301);
-	std::cout << "temperature: " << toStr(convert<Celsius>(t)) << "\n";
-	std::cout << "temperature: " << toStr(convert<Kelvin>(t)) << "\n";
-	std::cout << "temperature: " << toStr(convert<Fahrenheit>(t)) << "\n";
+	test("temperature:", asLabel(convert<Celsius>(t)));
+	test("temperature:", asLabel(convert<Kelvin>(t)));
+	test("temperature:", asLabel(convert<Fahrenheit>(t)));
 	return 0;
 
 }
