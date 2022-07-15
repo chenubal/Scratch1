12682360bb99f6c9df68445ba24344473080297e
Josef Heers <jheers@lavision.de>
2022-07-15 13:49:06 +0200

    Tagged types

--- VariantMatrix/Main.cpp
+++ VariantMatrix/Main.cpp
@@ -7,6 +7,23 @@
 #include <stdexcept>
 #include <array>
 
+template <typename T, typename Tag>
+class TaggedType
+{
+public:
+	using value_t = T;
+	explicit TaggedType(T const& v) : value(v) {}
+	explicit TaggedType(T&& v) : value(std::move(v)) {}
+	const T& operator*() const { return value; }
+private:
+	T value;
+};
+
+using Row = TaggedType<size_t, struct TagRow>;
+using Col = TaggedType<size_t, struct TagCol>;
+
+////////////////////////// Square Mat /////////////////////////////////////////
+
 template<class T>
 struct SqrMat
 {
@@ -22,8 +39,8 @@ struct SqrMat
 	auto begin() const { return data.cbegin(); }
 	auto end() const { return data.cend(); }
 
-	T& operator()(size_t i, size_t j) { return data[kth(i, j)]; }
-	T const& operator()(size_t i, size_t j) const { return data[kth(i, j)]; }
+	T& operator()(Col i, Row j) { return data[kth(i, j)]; }
+	T const& operator()(Col i, Row j) const { return data[kth(i, j)]; }
 
 	template<class S>
 	SqrMat<T>& operator=(S s) { for (auto &x : data) x = T(s); return *this; }
@@ -43,7 +60,7 @@ struct SqrMat
 
 	template<class S>
 	inline void komp(SqrMat<S> const& m) { if (size()!=m.size() )throw std::invalid_argument("non matching size");}
-	inline size_t kth(size_t i, size_t j) const { return j * rows + i; }
+	inline size_t kth(Col i, Row j) const { return *j * rows + *i; }
 protected:
 	size_t rows;
 	std::vector<T> data;
@@ -66,12 +83,12 @@ size_t size(Matrix const& m)
 }
 
 template<class T = double>
-T get(Matrix const& m, size_t i, size_t j)
+T get(Matrix const& m, Col i, Row j)
 {
 	return std::visit([=](auto const& mat) { return T(mat(i, j)); }, m);
 }
 
-void set(Matrix& m, size_t i, size_t j, double s)
+void set(Matrix& m, Col i, Row j, double s)
 {
 	auto v = overload(
 		[](auto&) {/*error*/},
@@ -145,87 +162,89 @@ bool sameType(Matrix const& m1, Matrix const& m2)
 	return m1.index() == m2.index();
 }
 
-
 int main()
 {
 	{
-		std::cout << "----------------------\n";
+		std::cout << "\n-------- Size, Get, Set --------------\n";
 		Matrix M1 = SqrMat<int>(5);
 		std::cout << "sz1=" << size(M1) << "\n";
-		std::cout << "x1=" << get(M1, 3, 3) << "\n";
-		set(M1, 3, 3, 14);
-		std::cout << "x1=" << get(M1, 3, 3) << "\n";
+		std::cout << "x1=" << get(M1, Col(3), Row(3)) << "\n";
+		set(M1, Col(3), Row(3), 14);
+		std::cout << "x1=" << get(M1, Col(3), Row(3)) << "\n";
 		M1 += 4;
-		std::cout << "x1=" << get(M1, 3, 3) << "\n";
+		std::cout << "x1=" << get(M1, Col(3), Row(3)) << "\n";
 	}
 
 	{
-		std::cout << "----------------------\n";
+		std::cout << "\n------ Ops +=, *=, +, *, convert, execute ----------------\n";
 		Matrix M1 = SqrMat<int>(5);
 		M1 += 2;
-		std::cout << "x1=" << get(M1, 3, 3) << "\n";
+		std::cout << "x1=" << get(M1, Col(3), Row(3)) << "\n";
 
 		auto M2 = convert(M1);
 		M2 += 1.1;
-		std::cout << "x2=" << get(M2, 3, 3) << "\n";
+		std::cout << "x2=" << get(M2, Col(3), Row(3)) << "\n";
 		auto M3 = M2;
 		M3 += M1;
-		std::cout << "x3=" << get(M3, 3, 3) << "\n";
+		std::cout << "x3=" << get(M3, Col(3), Row(3)) << "\n";
 		auto M4 = M3 + M2;
-		std::cout << "x4=" << get(M4, 3, 3) << "\n";
+		std::cout << "x4=" << get(M4, Col(3), Row(3)) << "\n";
 		auto M5 = M3 * M2;
 		M5 *= 0.1;
-		std::cout << "x5=" << get(M5, 3, 3) << "\n";
+		std::cout << "x5=" << get(M5, Col(3), Row(3)) << "\n";
 		execute(M5, [](double x)->double {return x * x; });
-		std::cout << "x5=" << get(M5, 3, 3) << "\n";
+		std::cout << "x5=" << get(M5, Col(3), Row(3)) << "\n";
 	}
+
 	{
-		std::cout << "\ndouble + int -----\n";
+		std::cout << "\n---- Types double + int -----\n";
 		Matrix M1 = SqrMat<double>(5, 100.1);
 		Matrix M2 = SqrMat<int>(5, 99);
 		auto M3 = M1 + M2;
-		std::cout << "m3=" << get(M3, 3, 3) << "\n";
+		std::cout << "m3=" << get(M3, Col(3), Row(3)) << "\n";
 		auto M4 = M2 + M1;
-		std::cout << "m4=" << get(M4, 3, 3) << "\n";
+		std::cout << "m4=" << get(M4, Col(3), Row(3)) << "\n";
 	}
+
 	{
-		std::cout << "\ndouble * int -----\n";
+		std::cout << "\n----Types double * int -----\n";
 		Matrix M1 = SqrMat<double>(5, 100.1);
 		Matrix M2 = SqrMat<int>(5, 99);
 		auto M3 = M1 * M2;
-		std::cout << "m3=" << get(M3, 3, 3) << "\n";
+		std::cout << "m3=" << get(M3, Col(3), Row(3)) << "\n";
 		auto M4 = M2 * M1;
-		std::cout << "m4=" << get(M4, 3, 3) << "\n";
+		std::cout << "m4=" << get(M4, Col(3), Row(3)) << "\n";
 	}
+
 	{
-		std::cout << "\nfloat + int -----\n";
+		std::cout << "\n----Types float + int -----\n";
 		Matrix M1 = SqrMat<float>(5, 100.1f);
 		Matrix M2 = SqrMat<int>(5, 99);
 		auto M3 = M1 + M2;
-		std::cout << "m3=" << get(M3, 3, 3) << "\n";
+		std::cout << "m3=" << get(M3, Col(3), Row(3)) << "\n";
 		auto M4 = M2 + M1;
-		std::cout << "m4=" << get(M4, 3, 3) << "\n";
+		std::cout << "m4=" << get(M4, Col(3), Row(3)) << "\n";
 	}
 	{
-		std::cout << "\nfloat + uint8 -----\n";
+		std::cout << "\n----Types float + uint8 -----\n";
 		Matrix M1 = SqrMat<float>(5, 100.1f);
 		Matrix M2 = SqrMat<uint8_t>(5, 254);
 		auto M3 = M1 + M2;
-		std::cout << "m3=" << get(M3, 3, 3) << "\n";
+		std::cout << "m3=" << get(M3, Col(3), Row(3)) << "\n";
 		auto M4 = M2 + M1;
-		std::cout << "m4=" << get(M4, 3, 3) << "\n";
+		std::cout << "m4=" << get(M4, Col(3), Row(3)) << "\n";
 	}
 	{
-		std::cout << "\nfloat + uint8 +double -----\n";
+		std::cout << "\n----Types float + uint8 + double -----\n";
 		Matrix M1 = SqrMat<float>(5, 100.1f);
 		Matrix M2 = SqrMat<uint8_t>(5, 254);
 		auto M3 = SqrMat<double>(5, 700);
 		try
 		{
 			auto M4 = M3 + M2 + M1;
-			std::cout << "m4=" << get(M4, 3, 3) << "\n";
+			std::cout << "m4=" << get(M4, Col(3), Row(3)) << "\n";
 			auto M5 = M3 + (M2 + M1);
-			std::cout << "m5=" << get(M5, 3, 3) << "\n";
+			std::cout << "m5=" << get(M5, Col(3), Row(3)) << "\n";
 		}
 		catch (std::exception& e)
 		{
