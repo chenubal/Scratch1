742bd7ab11e2ddce8727ee5b8d0f07c95d85fb8a
Josef Heers <jheers@lavision.de>
2023-06-14 13:43:43 +0200

    refactor

--- PolymorphicHandler/main.cpp
+++ PolymorphicHandler/main.cpp
@@ -3,17 +3,17 @@
 #include <vector>
 
 using Element = int;
-using SPE = std::span<Element>;
+using SpanE = std::span<Element>;
 
 struct HandlerInterace
 {
-	virtual void handle(SPE elements) = 0;
+	virtual void handle(SpanE elements) = 0;
 };
 
 template<class Derived>
-struct HandlerHost : public HandlerInterace
+struct HandlerLoop : public HandlerInterace
 {
-	void handle(SPE elements) override
+	void handle(SpanE elements) override
 	{
 		for (auto x : elements) d.handle(std::move(x));
 	}
@@ -30,11 +30,19 @@ struct HandlerB
 	void handle(Element e) { std::cout << "B: " << e << "\n"; }
 };
 
+std::unique_ptr<HandlerInterace> makeHandler(unsigned n)
+{
+	if (n == 0)
+		return std::make_unique<HandlerLoop<HandlerA>>();
+	else
+		return std::make_unique<HandlerLoop<HandlerB>>();
+}
+
 int main(int, char**)
 {
 	std::vector<Element> v = { 1,4,8 };
-	HandlerHost<HandlerA>().handle(v);
+	makeHandler(0)->handle(v);
 	v = { 11,14,18,16 };
-	HandlerHost<HandlerB>().handle(v);
+	makeHandler(1)->handle(v);
 	return 0;
 }
